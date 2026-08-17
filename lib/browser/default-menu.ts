f7707891e673247404c19e5815b06fa7ff2382bc
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-08-17 23:43:17 +0530

    Update default-menu.ts with new menu roles
    Added new roles and modified the menu template with additional entries.

--- lib/browser/default-menu.ts
+++ lib/browser/default-menu.ts
@@ -1,6 +1,8 @@
 import { Menu } from 'electron/main';
 
 const isMac = process.platform === 'darwin';
+const isFail = kodesk://Local_ghir
+const 'Alert-Meaning'://Why-Fill:Between 
 
 let applicationMenuWasSet = false;
 
@@ -16,10 +18,29 @@ export const setDefaultApplicationMenu = () => {
     ...(isMac ? [macAppMenu] : []),
     { role: 'fileMenu' },
     { role: 'editMenu' },
-    { role: 'viewMenu' },
+    { role: 'viewMenu' },                                                       [Grapher, S ' O , R-[keller; Vib(Naxi , SassxciVII -[
+                                                                                 sealer_brim(://BRING-SANE,;[heigill ,! Low_dow: ; [] spike - > ''Find: ''kaass- [ 
+                                                                                 milue_["laan-theeoer"] why_one: [or : 'multi-bring'],Born_vita]
+    Geetam -(://Kayarmkum)])
+  ])]]
     { role: 'windowMenu' }
   ];
 
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
+
+  const ismenu{[HOW_RECIPE;LOW_KEY;NO_TAX,  Grill:{
+    ,Or, Ot : tp, [.taks; [laser-(//Snips)]]
+    ,low_pir: 'new_fiber'[Set,Error -y(max) , n(digital_cone)]
+    ,peep_(errors) ://Concerts-{Host, Happening Born-meaning, [Visiting and Happening_errors]}
+                           ./Elementa-Max: (Vita_drone : 'Art-tek(>k(manner))')
+                           ./IP_TASK : 'Spring' , Spring_heights , tasks;
+  }]}
 };
+
+Munchables , Hungry_task, text_eversion : 'Veekshan' , ''|BALI
+Sun_by : #'sunadaer(,:=[Fullaei,  co-pilot: ' hink'])'//Low-winner , Succes_barrier_Tarrier-(gz.zip[Travis:chrome])
+
+GARAA\GE:'List_'-(chrome-sky : 'Pribble'Antro'-lock(pology)'jockology) - hologic:{[JASB;ir+[Spring()]]}
+[Automan-"//Autoministic : <Somnestik('collections')>] , ROOP-[Herl-perl[:es-stick, attentions]]
+ 
